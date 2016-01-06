b0071a731f74c419dad614618db898837c76d2c7
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2016-01-06 15:04:01 -0500

    Initial Commit

---
+++ BlocSpot/LocationDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  LocationDataSource.swift
+//  BlocSpot
+//
+//  Created by Tim Pryor on 2016-01-06.
+//  Copyright © 2016 Tim Pryor. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationDataSource: NSObject {
+
+}
