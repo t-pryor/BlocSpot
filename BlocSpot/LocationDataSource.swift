783a0c1cc997e3abd5de3e39255d8063f9f2901a
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2016-01-07 11:21:40 -0500

    Added properties and skeletons of a few methods to LocationDataSource.

--- BlocSpot/LocationDataSource.swift
+++ BlocSpot/LocationDataSource.swift
@@ -9,6 +9,71 @@
 import UIKit
 import CoreLocation
 
-class LocationDataSource: NSObject {
-
+class LocationDataSource: NSObject, CLLocationManagerDelegate {
+    
+    // singleton
+    static let sharedInstance = LocationDataSource()
+    var locationManager = CLLocationManager()
+    
+    
+    
+    override init() {
+        
+        super.init() // &*Investigate
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = 100.0
+        self.locationManager.delegate = self
+        
+        //&* change this
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        if let newLocation = locations.last {
+            //newLocation is most recent
+            //extract data to feed to GUI
+            // could return first if need to filter out
+            
+            
+            
+            
+            if newLocation.horizontalAccuracy < 0 {
+                // invalid accuracy
+                return
+            }
+            
+            // numbers in meters
+            if newLocation.horizontalAccuracy > 100 ||
+                newLocation.verticalAccuracy > 50 {
+                    // accuracy radius is so large, we don't want to use it
+                    return
+            }
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case .Authorized, .AuthorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+   
+    
+    
+    
 }
